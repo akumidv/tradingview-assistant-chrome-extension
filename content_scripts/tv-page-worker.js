a3062850b1ff6dc861aacb024ea91acba15fb015
Andrei Kuminov <akuminov@iondv.com>
2021-07-29 12:23:45 +1000

    init

---
+++ content_scripts/tv-page-worker.js
@@ -0,0 +1,9 @@
+/*
+ @license Copyright 2021 akumidv (https://github.com/akumidv/)
+ SPDX-License-Identifier: Apache-2.0
+*/
+'use strict';
+
+(async function() {
+  const waitForTimeout = async (timeout = 5000) => new Promise(resolve => setTimeout(resolve, timeout))
+})();
---
+++ popup/assistant.js
@@ -0,0 +1,5 @@
+/*
+ @license Copyright 2021 akumidv (https://github.com/akumidv/)
+ SPDX-License-Identifier: Apache-2.0
+*/
+'use strict';
