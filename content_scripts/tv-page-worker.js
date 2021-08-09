e372051528d8322c7f09bf4c078e1c62d8f11d05
Andrei Kuminov <akuminov@iondv.com>
2021-08-09 15:52:53 +1000

    click on react element

--- content_scripts/tv-page-worker.js
+++ content_scripts/tv-page-worker.js
@@ -6,4 +6,26 @@
 
 (async function() {
   const waitForTimeout = async (timeout = 5000) => new Promise(resolve => setTimeout(resolve, timeout))
+
+  function triggerME (el, eventType) {
+    var clickEvent = document.createEvent ('MouseEvents');
+    clickEvent.initEvent (eventType, true, true);
+    el.dispatchEvent (clickEvent);
+  }
+
+  function mouseClick (el) {
+    triggerME (el, "mouseover");
+    triggerME (el, "mousedown");
+    triggerME (el, "mouseup");
+    triggerME (el, "click");
+  }
+
+  // Example click on react object
+  // var element = document.querySelector('div[data-name="legend"] ')
+  // // CLICK
+  // mouseClick(element)
+  //
+  // // DOUBLE_CLICK
+  // mouseClick(element)
+  // mouseClick(element)
 })();
