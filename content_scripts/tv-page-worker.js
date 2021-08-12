4736218aab92256f08a0b3ff03916f5407e66fe8
Andrei Kuminov <akuminov@iondv.com>
2021-08-12 11:35:26 +1000

    update csv parser and prepare params for pine script

--- content_scripts/tv-page-worker.js
+++ content_scripts/tv-page-worker.js
@@ -11,9 +11,9 @@
   const STORAGE_KEY_PREFIX = 'iondv'
 
   chrome.runtime.onMessage.addListener(
-    async function(msg, sender, sendResponse) {
-      if(sender.tab || !msg.hasOwnProperty('action') || !msg.action) {
-        console.log('Not for action message received:', msg)
+    async function(request, sender, reply) {
+      if(sender.tab || !request.hasOwnProperty('action') || !request.action) {
+        console.log('Not for action message received:', request)
         return
       }
       if(workerStatus !== null) {
@@ -21,9 +21,9 @@
         return
       }
 
-      workerStatus = msg.action
+      workerStatus = request.action
 
-      switch (msg.action) {
+      switch (request.action) {
         case 'uploadSignals':
           await uploadFiles(parseTSSignalsAndGetMsg, `Please check if the ticker and timeframe are set like in the downloaded data and click on the parameters of the "iondvSignals" script to automatically enter new data on the chart.`, true)
           break;
@@ -32,46 +32,36 @@
           break;
         case 'clearAll':
           const clearRes = await storageClearAll()
-          alert(clearRes && clearRes.length ? `The data was deleted: ${clearRes.join(',')}` : 'There was no data in the storage')
+          alert(clearRes && clearRes.length ? `The data was deleted: \n${clearRes.map(item => '- ' + item).join('\n')}` : 'There was no data in the storage')
           break
         default:
-          console.log('None of realisation for signal:', msg)
+          console.log('None of realisation for signal:', request)
       }
       workerStatus = null
     }
   );
 
+  function parseCSVLine(text) {
+    return text.match( /\s*(\".*?\"|'.*?'|[^,]+)\s*(,|$)/g ).map(function (text) {
+      let m;
+      if (m = text.match(/^\s*\"(.*?)\"\s*,?$/)) return m[1]; // Double Quoted Text
+      if (m = text.match(/^\s*'(.*?)'\s*,?$/)) return m[1]; // Single Quoted Text
+      if (m = text.match(/^\s*(true|false)\s*,?$/)) return m[1] === "true"; // Boolean
+      if (m = text.match(/^\s*((?:\+|\-)?\d+)\s*,?$/)) return parseInt(m[1]); // Integer Number
+      if (m = text.match(/^\s*((?:\+|\-)?\d*\.\d*)\s*,?$/)) return parseFloat(m[1]); // Floating Number
+      if (m = text.match(/^\s*(.*?)\s*,?$/)) return m[1]; // Unquoted Text
+      return text;
+    } );
+  }
+
   function parseCSV2JSON(s, sep=',') {
-    const newline = /\r\n|\r|\n/g;
-    let a = s.split(newline);
-    let csv = []
-    for(let line of a){
-      if(!line) continue
-      let f = line.split(sep = sep || ",")
-      for (let x = f.length - 1; x >= 0; x--) {
-        if (f[x].replace(/"\s+$/, '"').charAt(f[x].length - 1) == '"') {
-          const tl = f[x].replace(/^\s+"/, '"')
-          if (tl.length > 1 && tl.charAt(0) == '"') {
-            f[x] = f[x].replace(/^\s*"|"\s*$/g, '').replace(/""/g, '"');
-          } else if (x) {
-            f.splice(x - 1, 2, [f[x - 1], f[x]].join(sep));
-          } else {
-            f = f.shift().split(sep).concat(f);
-          }
-        } else {
-          f[x].replace(/""/g, '"');
-        }
-      }
-      csv.push(f);
-    }
-    const JSONData = []
-    if(!csv || csv.length <= 1)
-      return JSONData
+    const csv = s.split(/\r\n|\r|\n/g).filter(item => item).map(line => parseCSVLine(line))
+    if(!csv || csv.length <= 1) return []
     const headers = csv[0].map(item => item.toLowerCase())
-    csv.slice(1).forEach((line) => {
+    const JSONData = csv.slice(1).map((line) => {
       const lineObj = {}
       line.forEach((value, line_index) => lineObj[headers[line_index]] = value)
-      JSONData.push(lineObj)
+      return lineObj
     })
     return JSONData;
   }
@@ -95,15 +85,12 @@
 
   async function parseCSVFile (fileData) {
     return new Promise((resolve, reject) => {
-      const isCSV = fileData.name.toLowerCase().endsWith('.csv')
-      console.log(fileData.name, isCSV)
-      if(!isCSV)
-        return reject(`please upload correct file.`)
       const CSV_FILENAME = fileData.name
+      const isCSV = CSV_FILENAME.toLowerCase().endsWith('.csv')
+      if(!isCSV) return reject(`please upload correct file.`)
       const reader = new FileReader();
       reader.addEventListener('load', async (event) => {
-        if(!event.target.result)
-          return reject(`there error when loading content from the file ${CSV_FILENAME}`)
+        if(!event.target.result) return reject(`there error when loading content from the file ${CSV_FILENAME}`)
         const CSV_VALUE = event.target.result
         try {
           const csvData = parseCSV2JSON(CSV_VALUE)
@@ -111,7 +98,7 @@
             return resolve(csvData)
         } catch (err) {
           console.error(err)
-          return resolve(`CSV parsing error: ${err.message}`)
+          return reject(`CSV parsing error: ${err.message}`)
         }
         return reject(`there is no data in the file`)
       })
@@ -119,24 +106,36 @@
     });
   }
 
-  function convertToTimeframe(data, tfValues, tfType) {
+  function shiftToTimeframe(data, tfValues, tfType) {
     switch (tfType.toLowerCase()) {
+      case 'd':
+        return data.map(dt => {
+          console.log('#d>',tfValues, dt.getUTCDate() % tfValues, dt)
+          if(dt.getUTCDate() % tfValues !== 0) {
+            dt.setUTCDate(dt.getUTCDate() - dt.getUTCDate() % tfValues)
+          }
+          dt.setUTCHours(0, 0, 0, 0)
+          console.log('#d<',tfValues, dt.getUTCDate() % tfValues, dt)
+          return dt
+        })
       case 'h':
         return data.map(dt => {
-          if(dt.getUTCHours() % tfValues !== 0) {
-            const tfCoreсtion = dt.getUTCHours() - dt.getUTCHours() % tfValues
-            dt.setUTCHours(tfCoreсtion, 0, 0, 0)
-            return dt
-          }
+          console.log('#h>',tfValues, dt.getUTCHours() % tfValues, dt)
+          if(dt.getUTCHours() % tfValues !== 0)
+            dt.setUTCHours(dt.getUTCHours() - dt.getUTCHours() % tfValues, 0, 0, 0)
+          else
+            dt.setUTCMinutes( 0, 0, 0)
+          console.log('#h<',tfValues, dt.getUTCHours() % tfValues, dt)
           return dt
         })
       case 'm':
         return data.map(dt => {
-          if(dt.getUTCMinutes() % tfValues !== 0) {
-            const tfCoreсtion = dt.getUTCMinutes() - dt.getUTCMinutes() % tfValues
-            dt.setUTCMinutes(tfCoreсtion, 0, 0)
-            return dt
-          }
+          console.log('#m>',tfValues, dt.getUTCMinutes() % tfValues, dt)
+          if(dt.getUTCMinutes() % tfValues !== 0)
+            dt.setUTCMinutes( dt.getUTCMinutes() - dt.getUTCMinutes() % tfValues, 0, 0)
+          else
+            dt.setUTCSeconds( 0, 0)
+          console.log('#m<',tfValues, dt.getUTCMinutes() % tfValues, dt)
           return dt
         })
       default:
@@ -161,61 +160,63 @@
       const headers = Object.keys(csvData[0])
       const missColumns = ['timestamp', 'ticker', 'timeframe', 'signal'].filter(columnName => !headers.includes(columnName))
       if(missColumns && missColumns.length)
-        return `  - ${fileData.name}: There is no column(s) "${missColumns.join(', ')}" in CSV. Please add all necessary columns to CSV like showed in the template.\n`
+        return `  - ${fileData.name}: There is no column(s) "${missColumns.join(', ')}" in CSV. Please add all necessary columns to CSV like showed in the template. Uploading canceled.\n`
       const tickersAndTFSignals = {}
-      for(let row of csvData) {
-        if(row['timestamp'] && row['signal'] && row['ticker'] &&
-          row['timeframe'] && row['timeframe'].length >= 2) {
+      for(let row of csvData) { // Prepare timestamp arrays
+        if(row['timestamp'] && row['signal'] && row['ticker'] && row['timeframe'] && row['timeframe'].length >= 2) {
           try {
             const [tfVal, tfType] = parseTF(row['timeframe'])
-            console.log(tfVal, tfType, !['h','m'].includes(tfType) || !(tfVal > 0))
-            if(!['h','m'].includes(tfType) || !(tfVal > 0))
-              return `  - ${fileData.name}: only minute(m) and hour(h) timeframes are supported. There is a timeframe '${row['timeframe']}' in the file.\n`
-            const tktfName = `${row['ticker']}#${tfVal}${tfType}`.toLowerCase()
+            if(!['h', 'm', 'd'].includes(tfType) || !(tfVal > 0))
+              return `  - ${fileData.name}: only minute(m) and hour(h) timeframes are supported. There is a timeframe '${row['timeframe']}' in the file. Uploading canceled.\n`
+            const tktfName = `${row['ticker']}::${tfVal}${tfType}`.toLowerCase()
             if(!tickersAndTFSignals.hasOwnProperty(tktfName))
               tickersAndTFSignals[tktfName] = {tsBuy: [], tsSell: []}
-            if(row['signal'].toLowerCase().includes('buy'))
-              tickersAndTFSignals[tktfName].tsBuy.push(new Date(row['timestamp']))
-            else if (row['signal'].toLowerCase().includes('sell'))
-              tickersAndTFSignals[tktfName].tsSell.push(new Date(row['timestamp']))
-          } catch {}
+            const ts = new Date(row['timestamp'])
+            if(!isNaN(ts.getTime())) {
+              if(row['signal'].toLowerCase().includes('buy'))
+                tickersAndTFSignals[tktfName].tsBuy.push(ts)
+              else if (row['signal'].toLowerCase().includes('sell'))
+                tickersAndTFSignals[tktfName].tsSell.push(ts)
+              console.log(`Timestamp ${row['timestamp']}  ${ts}`)
+            } else {
+              console.log(`Timestamp ${row['timestamp']} ${typeof(row['timestamp'])} isn't valid`)
+            }
+
+
+          } catch (err) {
+            console.error(err)
+          }
         }
       }
-      let fileMessage = ``
-      const allTickersAndTF = Object.keys(tickersAndTFSignals)
-      // for(let tktfName of allTickersAndTF) { // TODO convert timeframes
-      //   const tsBuyPrepared = convertToTimeframe(tickersAndTFSignals[tktfName].tsBuy, TIME_FRAME_VAL, TIME_FRAME_TYPE)
-      //   const tsBuyConverted = tsBuyPrepared.map(dt => dt.getTime())
-      //   let tsBuyFiltered =  tsBuyConverted.filter((item, idx) => tsBuyConverted.indexOf(item) == idx)
-      //   const buyDoubletsNumber = tsBuyConverted.length - tsBuyFiltered.length
-      //   const tsBuyLimited = tsBuyFiltered //tsBuyFiltered.length > 1500 ? tsBuyFiltered.slice(tsBuyFiltered.length - 1500) : tsBuyFiltered
-      //   tsBuyStr = !tsBuyLimited ? '' : tsBuyLimited.join(',')
-      //
-      //   const tsSellPrepared = convertToTimeframe(tickersAndTFSignals[tktfName].tsSell, TIME_FRAME_VAL, TIME_FRAME_TYPE)
-      //   const tsSellConverted = tsSellPrepared.map(dt => dt.getTime())
-      //   let tsSellFiltered =  tsSellConverted.filter((item, idx) => tsSellConverted.indexOf(item) == idx)
-      //   const sellDoubletsNumber = tsSellConverted.length - tsSellFiltered.length
-      //   const tsSellLimited = tsSellFiltered //tsSellFiltered.length > 1500 ? tsSellFiltered.slice(tsSellFiltered.length - 1500) : tsSellFiltered
-      //   tsSellStr = !tsSellLimited ? '' : tsSellLimited.join(',')
-      //
-      //   const tsData = {buy: tsBuyStr, sell: tsSellStr, loadData: (new Date()).toISOString()}
-      //   await storageSetTickerTF(tktfName, tsData)
-      //   console.log(tktfName, TIME_FRAME_VAL, TIME_FRAME_TYPE, `Timestamps:  ${tsBuyFiltered.length + tsSellFiltered.length} (prepared to ${tsBuyLimited.length + tsSellLimited.length}) uploaded out of ${sourceData.length}, param srt len ${tsBuyStr.length + tsSellStr.length}`)
-      //   fileMessage += `  - for ticker ${tktfName}, there are ${tsBuyConverted.length + tsSellConverted.length} timestamps (prepared for uploading ${tsBuyLimited.length + tsSellLimited.length}). ` +
-      //     `${sellDoubletsNumber === 0  && buyDoubletsNumber === 0 ? '' : 'Removed doublets ' + (buyDoubletsNumber + sellDoubletsNumber) + ' of signals in the same timeframe.'}\n`
-      //   //   `${(tsBuyFiltered.length + tsSellFiltered.length) >= 1500 ? '    The number of timestamps exceeds the limit of 1500 pieces. Only the last 1500 timestamps will be shown.' : ''}\n`
-      // }
-      return `- ${fileMessage}Data saved in storage.\n`
-
-
+      let msgArr = []
+      for(let tktfName of Object.keys(tickersAndTFSignals)) {
+        try {
+          const tf = tktfName.split('::').pop()
+          const [tfVal, tfType] = parseTF(tf)
+          if(!tfVal || !tfType) continue
+          const buyArr = shiftToTimeframe(tickersAndTFSignals[tktfName].tsBuy, tfVal, tfType)
+          const buyConv = buyArr.map(dt => dt.getTime())
+          console.log('buyConv', tf, buyConv)
+          const sellArr = shiftToTimeframe(tickersAndTFSignals[tktfName].tsSell,  tfVal, tfType)
+          const sellConv = sellArr.map(dt => dt.getTime())
+          console.log('sellConv', tf, sellConv)
+          await storageSetKeys(tktfName,  {buy: buyConv.filter((item, idx) => buyConv.indexOf(item) === idx).join(','),
+                                                sell: sellConv.filter((item, idx) => sellConv.indexOf(item) === idx).join(','),
+                                                loadData: (new Date()).toISOString()})
+          console.log(tktfName, tfVal, tfType, `Timestamps:  ${buyConv.length + sellConv.length}`)
+          msgArr.push(`${tktfName} (${buyConv.length + sellConv.length})`)
+        } catch (err) {
+          console.error(err)
+        }
+      }
+      return `- ${fileData.name}. Timestamps saved for tickers: ${msgArr.join(', ')}. Data saved in storage.\n`
     } catch (err) {
       console.error(fileData.name)
       console.error(err)
       return `- ${fileData.name}: ${err.message}\n`
     }
-    console.log('parseTSSignalsAndGetMsg filename', fileData.name)
-    return `- ${fileData.name}: data saved in storage\n`
   }
+
   async function parsStrategyParamsAndGetMsg (filename) { // TODO
     console.log('parsStrategyParamsAndGetMsg filename', filename)
     return filename
